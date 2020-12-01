fe3488f10fa4acb836c2bd2b9fec121e4364d7f6
Reuben Tan <reubentjl@gmail.com>
2020-12-02 01:29:24 +0800

    update ui

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,21 @@ import BusesDataService from "../services/BusesService";
 import { Service } from "../services/DbService";
 import { getBoundsOfDistance } from "geolib";
 
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/Button';
+import SearchIcon from '@material-ui/icons/Search';
+import CachedIcon from '@material-ui/icons/Cached';
+import LocationSearchingIcon from '@material-ui/icons/LocationSearching';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import DirectionsBusIcon from '@material-ui/icons/DirectionsBus';
+
 const Home = () => {
   const [busStopCode, setBusStopCode] = useState("");
   // const [coord, setCoord] = useState({
@@ -55,10 +70,15 @@ const Home = () => {
   }
 
   const handleNearbyClick = () => {
-    setBusStopCode("");
-    setNearestBusStops({});
-    setBusStopArrival({});
-    navigator.geolocation.getCurrentPosition(updateCoord);
+    if (navigator.geolocation) {
+      setBusStopCode("");
+      setNearestBusStops({});
+      setBusStopArrival({});
+      navigator.geolocation.getCurrentPosition(updateCoord, coordError);
+    } else {
+      alert("Unable to retrieve current location");
+    }
+    
   }
 
   const handleReloadClick = async () => {
@@ -85,6 +105,25 @@ const Home = () => {
     const arrivalDict = await updateArrivals(busStopDict);
     setNearestBusStops(busStopDict);
     setBusStopArrival(arrivalDict);
+  }
+
+  const coordError = (error) => {
+    var msg = '';
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        msg = "User denied the request for Geolocation."
+        break;
+      case error.POSITION_UNAVAILABLE:
+        msg = "Location information is unavailable."
+        break;
+      case error.TIMEOUT:
+        msg = "The request to get user location timed out."
+        break;
+      case error.UNKNOWN_ERROR:
+        msg = "An unknown error occurred."
+        break;
+    }
+    alert(msg);
   }
 
   const updateNearestBusStops = async (bounds) => {
@@ -125,64 +164,104 @@ const Home = () => {
       </>
     ) : (
       <>
-        <h1></h1>
-        <button
-          type="button"
-          onClick={handleNearbyClick}
-        >
-          Nearby
-        </button>
-        <input
-          type="text"
-          placeholder="Enter busstop number"
-          value={busStopCode}
-          onChange={onChangeBusStopCode}
-        />
-        <button
-          type="button"
-          onClick={arrivalById}
-        >
-          Search
-        </button>
-        <button
-          type="button"
-          onClick={handleReloadClick}
-        >
-          Reload
-        </button>
-
-        <br/>
+        <Box m={1}>
+          <Grid
+            container
+            spacing={1}
+            alignItems='center'
+          >
+            <Grid item xs={5}>
+              <TextField
+                variant="outlined"
+                label="Busstop Code"
+                value={busStopCode}
+                onChange={onChangeBusStopCode}
+              />
+            </Grid>
+            <Grid item xs={2}>
+              <IconButton onClick={arrivalById}>
+                <SearchIcon/>
+              </IconButton>
+            </Grid>
+            <Grid item xs={2}>
+              <IconButton onClick={handleReloadClick}>
+                <CachedIcon/>
+              </IconButton>
+            </Grid>
+            <Grid item xs={2}>
+              <IconButton onClick={handleNearbyClick}>
+                <LocationSearchingIcon/>
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Box>
 
         {busStopArrival && Object.keys(busStopArrival).map((key, index) =>(
-          <div key={key}>
-          {nearestBusStops[key] &&
-          (
-            <h2>{nearestBusStops[key].Description} - {nearestBusStops[key].RoadName} ({nearestBusStops[key].BusStopCode})</h2>
-          )
-          }
-          {nearestBusStops[key] && busStopArrival[key].map((service, index) => (
-            <div key={service.ServiceNo + key}>
-              <h3>{service.ServiceNo}</h3>
-              <ul>
-                {service.NextBus.EstimatedArrival &&
-                  <li key={index.toString() + "1"}>
-                    {service.NextBus.EstimatedArrival}
-                  </li>
-                }
-                {service.NextBus2.EstimatedArrival &&
-                  <li key={index.toString() + "2"}>
-                    {service.NextBus2.EstimatedArrival}
-                  </li>
-                }
-                {service.NextBus3.EstimatedArrival &&
-                  <li key={index.toString() + "3"}>
-                    {service.NextBus3.EstimatedArrival}
-                  </li>
-                }
-              </ul>
-            </div>
-          ))}
-          </div>
+          <Box key={key} m={1}>
+          <Card variant="outlined">
+            <CardContent>
+              {nearestBusStops[key] &&
+              (
+                <Typography variant="h5" component="h2">
+                  {nearestBusStops[key].Description} - {nearestBusStops[key].RoadName} ({nearestBusStops[key].BusStopCode})
+                </Typography>
+              )
+              }
+              <List>
+                {nearestBusStops[key] && busStopArrival[key].map((service, index) => (
+                  <ListItem disableGutters={true} key={service.ServiceNo}>
+                    <Box>
+                      <h3>{service.ServiceNo}</h3>
+                      <Grid container spacing={1}>
+                        {service.NextBus.EstimatedArrival &&
+                          <Grid item key={index.toString() + "1"} xs={4}>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            size="large"
+                            startIcon={<DirectionsBusIcon />}
+                          >
+                            {Math.round((((new Date() - Date.parse(service.NextBus.EstimatedArrival)) % 86400000) % 3600000) / 60000)}
+                            {}
+                          </Button>
+                          </Grid>
+                        }
+                        {service.NextBus2.EstimatedArrival &&
+                          <Grid item key={index.toString() + "1"} xs={4}>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            size="large"
+                            startIcon={<DirectionsBusIcon />}
+                          >
+                            {Math.round((((new Date() - Date.parse(service.NextBus2.EstimatedArrival)) % 86400000) % 3600000) / 60000)}
+                            {}
+                          </Button>
+                          </Grid>
+                        }
+                        {service.NextBus3.EstimatedArrival &&
+                          <Grid item key={index.toString() + "1"} xs={4}>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            size="large"
+                            startIcon={<DirectionsBusIcon />}
+                          >
+                            {Math.round((((new Date() - Date.parse(service.NextBus3.EstimatedArrival)) % 86400000) % 3600000) / 60000)}
+                            {}
+                          </Button>
+                          </Grid>
+                        }
+                      </Grid>
+                    </Box>
+                  </ListItem>
+                ))}
+              </List>
+              
+            </CardContent>
+          </Card>
+          
+          </Box>
         ))}
       </>
     )}
