c2f59f0cfdbd502a470bda5bed8e73d81d86e2e1
Reuben Tan <reubentjl@gmail.com>
2020-12-16 01:08:07 +0800

    update HomePage routing for App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './App.css';
 
-import BusesList from "./components/BusesList";
-import BusStopsList from "./components/BusStopsList";
-import Home from "./components/Home";
+import BusList from "./components/BusList";
+import Search from './components/Search';
+import HomePage from './containers.js/HomePage';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path={["/"]} component={Home}/>
-        <Route exact path={["/buses"]} component={BusesList}/>
-        <Route exact path={["/busStops"]} component={BusStopsList}/>
+        <Route exact path={["/"]} component={HomePage}/>
       </Switch>
     </div>
   );
